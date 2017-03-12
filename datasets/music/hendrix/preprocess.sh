26ff52b01379c566a72dc74a0e7992a28ff7efaf
zvk <drawordoodle@gmail.com>
2017-03-12 04:29:27 -0400

    removed try

--- datasets/music/hendrix/preprocess.sh
+++ datasets/music/hendrix/preprocess.sh
@@ -4,12 +4,11 @@ echo "Converting from OGG to 16Khz, 16bit mono-channel WAV"
 # Next line with & executes in a forked shell in the background. That's parallel and not recommended.
 # Remove if causing problem
 #for file in "$DL_PATH"*_64kb.mp3; do ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" _64kb.mp3`.wav" & done 
-try:
-	for file in "$SCRIPTPATH"*.ogg; do
-		ffmpeg -i "$file" -ar 16000 -ac 1 "$SCRIPTPATH""`basename "$file" .ogg`.wav"
-	done 
-	echo "Cleaning up"
-	rm "$SCRIPTPATH"*.ogg
+for file in "$SCRIPTPATH"*.ogg; do
+	ffmpeg -i "$file" -ar 16000 -ac 1 "$SCRIPTPATH""`basename "$file" .ogg`.wav"
+done 
+echo "Cleaning up"
+rm "$SCRIPTPATH"*.ogg
 
 echo "Preprocessing"
 python preprocess.py "$SCRIPTPATH"
