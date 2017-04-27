377494833f9e551fca91067c22fe87e4e24ae4bf
zvk <drawordoodle@gmail.com>
2017-04-27 13:25:49 -0500

    fixed typos

--- datasets/music/new_experiment.py
+++ datasets/music/new_experiment.py
@@ -8,7 +8,8 @@ DATASET_NAME = str(sys.argv[1])
 DOWNLOAD_DIR = str(sys.argv[2])
 #create the 
 print "creating directory for", DATASET_NAME
-os.makedirs(DATASET_NAME)
+DATASET_DIR = os.path.join(PWD, DATASET_NAME)
+os.makedirs(DATASET_DIR)
 #move samples from directory to use dataset name
 print "moving samples"
 types = {'wav', "mp3"}
@@ -16,11 +17,10 @@ for t in types:
     os.system('mv {}/*.{} ./{}/'.format(DOWNLOAD_DIR, t, DATASET_NAME))
 #run proprocess
 print "preprocessing"
-OUTPUT_DIR=os.path.join(DATASET_NAME, "parts")
+OUTPUT_DIR=os.path.join(DATASET_DIR, "parts")
 os.makedirs(OUTPUT_DIR)
-print os.path.join(PWD, '/preprocess_file_list.txt')
 # Step 1: write all filenames to a list
-with open(os.path.join(PWD, '/preprocess_file_list.txt'), 'w') as f:
+with open(os.path.join(PWD, 'preprocess_file_list.txt'), 'w') as f:
     for dirpath, dirnames, filenames in os.walk(DATASET_NAME):
         for filename in filenames:
             if filename.endswith(".wav") or filename.endswith("mp3"):
@@ -29,24 +29,20 @@ with open(os.path.join(PWD, '/preprocess_file_list.txt'), 'w') as f:
 # Step 2: concatenate everything into one massive wav file
 print "concatenate all files"
 os.system('pwd')
-os.system("ffmpeg -f concat -safe 0 -i /preprocess_file_list.txt {}/preprocess_all_audio.wav".format(OUTPUT_DIR))
+os.system("ffmpeg -f concat -safe 0 -i {}/preprocess_file_list.txt {}/preprocess_all_audio.wav".format(PWD, OUTPUT_DIR))
 audio = "preprocess_all_audio.wav"
 print "get length"
 # # get the length of the resulting file
 length = float(subprocess.check_output('ffprobe -i {}/{} -show_entries format=duration -v quiet -of csv="p=0"'.format(OUTPUT_DIR, audio), shell=True))
 print length, "DURATION"
-# reverse the audio file
-if sys.argv[2] == True:
-    os.system("sox preprocess_all_audio.wav reverse_preprocess_audio.wav reverse")
-    audio = "reverse_preprocess_audio.wav"
 print "print big file into chunks"
 # # Step 3: split the big file into 8-second chunks
 for i in xrange((int(length)//8 - 1)/3):
     os.system('ffmpeg -ss {} -t 8 -i {}/{} -ac 1 -ab 16k -ar 16000 {}/p{}.flac'.format(i, OUTPUT_DIR, audio, OUTPUT_DIR, i))
 print "clean up"
 # # Step 4: clean up temp files
-#os.system('rm {}/preprocess_all_audio.wav'.format(OUTPUT_DIR))
-os.system('rm {}/preprocess_file_list.txt'.format(OUTPUT_DIR))
+os.system('rm {}/preprocess_all_audio.wav'.format(OUTPUT_DIR))
+os.system('rm {}/preprocess_file_list.txt'.format(PWD))
 print 'save as .npy'
 __RAND_SEED = 123
 def __fixed_shuffle(inp_list):
@@ -73,9 +69,9 @@ length = len(np_arr)
 train_size = int(np.floor(length * .88)) # train
 test_size = int(np.floor(length * .06)) # test
 
-np.save(join(DATASET_NAME,'all_music.npy'), np_arr)
-np.save(join(DATASET_NAME,'music_train.npy'), np_arr[:train_size])
-np.save(join(DATASET_NAME,'music_valid.npy'), np_arr[train_size:train_size + test_size])
-np.save(join(DATASET_NAME,'music_test.npy'), np_arr[train_size + test_size:])
+np.save(os.path.join(DATASET_DIR,'all_music.npy'), np_arr)
+np.save(os.path.join(DATASET_DIR,'music_train.npy'), np_arr[:train_size])
+np.save(os.path.join(DATASET_DIR,'music_valid.npy'), np_arr[train_size:train_size + test_size])
+np.save(os.path.join(DATASET_DIR,'music_test.npy'), np_arr[train_size + test_size:])
 
 #pass dataset name through two_tier.py || three_tier.py to datasets.py
