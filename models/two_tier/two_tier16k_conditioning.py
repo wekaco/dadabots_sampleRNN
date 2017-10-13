f795d4e61f3a754b2f989039de68c3c90fb98e9c
Cortexelus <emperorcj@gmail.com>
2017-10-13 04:36:36 -0400

    local conditioning; get training to work

--- models/two_tier/two_tier16k_conditioning.py
+++ models/two_tier/two_tier16k_conditioning.py
@@ -297,9 +297,9 @@ def frame_level_rnn(input_sequences, h0, reset, features):
     # Fuse previous frame and the current local conditioning features
     rnn_inp = lib.ops.Linear(
         'FrameLevel.rnn_inp_fusion',
-        [FRAME_SIZE, FRAME_SIZE],
+        FRAME_SIZE,
         DIM,
-        [frames, features],
+        frames,
         initialization='he',
         weightnorm=WEIGHT_NORM
     )
