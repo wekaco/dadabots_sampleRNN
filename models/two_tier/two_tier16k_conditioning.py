ea90bc08863130902d58c8dba4db97d781ba213e
Cortexelus <emperorcj@gmail.com>
2017-10-13 06:43:51 -0400

    local conditioning; get generating to work

--- models/two_tier/two_tier16k_conditioning.py
+++ models/two_tier/two_tier16k_conditioning.py
@@ -545,7 +545,7 @@ fixed_rand_h0 = fixed_rand_h0.astype('float32')
 # warning this may be break if sampling more than 8 seconds
 # in which case, you want to start concatenating bits together
 def frame_features_for_generating():
-    data = get_feature_data(WHICH_SET)
+    data = datasets.dataset_conditioning.get_feature_data(WHICH_SET)
 
     ## borrow from the first 8 second chunk
     # yes some things are hard coded
