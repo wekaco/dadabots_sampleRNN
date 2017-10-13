0d25344f46a030ff0537357200d22b0ec8895b79
Cortexelus <emperorcj@gmail.com>
2017-10-13 04:39:33 -0400

    local conditioning; get training to work

--- models/two_tier/two_tier16k_conditioning.py
+++ models/two_tier/two_tier16k_conditioning.py
@@ -298,7 +298,7 @@ def frame_level_rnn(input_sequences, h0, reset, features):
     rnn_inp = lib.ops.Linear(
         'FrameLevel.rnn_inp_fusion',
         FRAME_SIZE,
-        DIM,
+        FRAME_SIZE,
         frames,
         initialization='he',
         weightnorm=WEIGHT_NORM
@@ -322,7 +322,7 @@ def frame_level_rnn(input_sequences, h0, reset, features):
     if RNN_TYPE == 'GRU':
         rnns_out, last_hidden = lib.ops.stackedGRU('FrameLevel.GRU',
                                                    N_RNN,
-                                                   DIM,
+                                                   FRAME_SIZE,
                                                    DIM,
                                                    rnn_inp,
                                                    h0=h0,
@@ -331,7 +331,7 @@ def frame_level_rnn(input_sequences, h0, reset, features):
     elif RNN_TYPE == 'LSTM':
         rnns_out, last_hidden = lib.ops.stackedLSTM('FrameLevel.LSTM',
                                                     N_RNN,
-                                                    DIM,
+                                                    FRAME_SIZE,
                                                     DIM,
                                                     rnn_inp,
                                                     h0=h0,
