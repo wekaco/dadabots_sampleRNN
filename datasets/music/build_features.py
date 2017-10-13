f372764e5d48dc0e42cf3427fca95400d40f298b
Cortexelus <emperorcj@gmail.com>
2017-10-13 00:18:06 -0400

    local conditioning, testing, fix bug

--- datasets/music/build_features.py
+++ datasets/music/build_features.py
@@ -2,6 +2,7 @@
 # The feature matrix doesn't need to be upsampled to the sample rate
 # However long the matrix is, we assume it matches the length of the WAV
 # So you can use any frame_rate (hop_size)
+import numpy as np
 def build_features(filename):
     features = np.array(range(0,1000))/1000
     return features
